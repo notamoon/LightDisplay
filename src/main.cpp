370249ef475081ed71ce2b2ee81e41fc5b15ab02
notamoon <mylessamuelharris@gmail.com>
2021-12-04 14:33:20 -0800

    removing http for now

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WebSocketsClient.h>
 
 #include "ArduinoJson.h"
 #include "Adafruit_NeoPixel.h"
@@ -13,113 +14,67 @@ Adafruit_NeoPixel strip(conf::displayWidth * conf::displayHeight, conf::dataPin,
 
 LightRenderer renderer(strip, conf::displayWidth, conf::displayHeight);
 
-void requestMessage();
+WebSocketsClient client;
+
+void clientDataReceived(WStype_t, uint8_t*, size_t);
 
 void setup() {
-    Serial.begin(115200);
+    // LIGHT RENDERER SETUP ========================
+    renderer.setup();
 
-    Serial.print("Connecting to ");
-    Serial.println(conf::networkSSID);
+    renderer.setInverted(conf::inverted);
+    renderer.setBrightness(255);
 
+    renderer.update();
+    renderer.render();
+    // =============================================
+
+    // INTERNET CONNECTION =========================
     WiFi.mode(WIFI_STA);
     WiFi.begin(conf::networkSSID, conf::networkPassword);
 
+    renderer.setFont(Fonts::FONT1);
+    renderer.setColor(Color::RED);
+
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+        renderer.scrollText("Connecting to internet...", 0, 0, 1, -1, Direction::HORIZONTAL, 1);
+
+        renderer.update();
+        renderer.render();
+
+        delay(50);
     }
+    // =============================================
 
-    Serial.println("");
-    Serial.println("WiFi connected");
+    // WEBSOCKET CLIENT ============================
+    client.begin(conf::serverHost, conf::serverPort, conf::dataURL);
 
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    renderer.setup();
-
-    renderer.setInverted(false);
-
-    renderer.setBrightness((int)(.25 * 255));
-
-    Serial.begin(115200);
+    client.onEvent(clientDataReceived);
+    client.setReconnectInterval(5000);
+    client.enableHeartbeat(15000, 3000, 2);
+    // =============================================
 }
 
-unsigned long lastMessageRequest = 0;
-unsigned long requestDelay = 5000;
-
-unsigned int color = 0;
-string message;
-
 void loop() {
-    if (lastMessageRequest + requestDelay < millis()) {
-        Serial.println("Sending message request.");
-
-        requestMessage();
-        lastMessageRequest = millis();
-    }
-
-    renderer.clear();
-
-    renderer.setColor(Color::CYAN);
-
-    renderer.drawArea(0, 0, 30, 20);
-
-    renderer.setFont(Fonts::FONT2);
-
-    renderer.setColor(color);
-    renderer.scrollText(message, 1, 4, 2, 28, Direction::HORIZONTAL, 2);
-
-    // Required
     renderer.update();
     renderer.render();
 }
 
-void requestMessage() {
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("Unable to connect to server!");
-        return;
+void clientDataReceived(WStype_t type, uint8_t* payload, size_t length) {
+//// webSocket.sendTXT("message here");
+//// webSocket.sendBIN(payload, length);
+    switch(type) {
+        case WStype_DISCONNECTED:
+            break;
+        case WStype_CONNECTED:
+            break;
+        case WStype_TEXT:
+            break;
+        case WStype_BIN:
+            break;
+        case WStype_PING:
+            break;
+        case WStype_PONG:
+            break;
     }
-
-    WiFiClient client;
-
-    HTTPClient http;
-
-    Serial.println("Sending HTTP Request");
-
-    if (!http.begin(client, conf::serverURL)) {
-        Serial.println("WiFi is not connected.");
-        return;
-    }
-
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-
-    // httpCode will be negative on error
-    if (httpCode > 0) {
-
-        if (httpCode == HTTP_CODE_OK) {
-            String payload = http.getString();
-
-            DynamicJsonDocument document(400);
-
-            DeserializationError error = deserializeJson(document, payload);
-
-            if (error) {
-                Serial.print("Failed to deserialize JSON, error code: ");
-                Serial.println(error.f_str());
-                return;
-            }
-
-            //{"id":1,"message":"test","color":[255,255,255],"scroll":true,"speed":75,"displayWidth":20,"displayHeight":30,"brightness":0.25}
-
-            message = document["message"].as<const char*>();
-
-            color = Adafruit_NeoPixel::Color(document["color"][0], document["color"][1], document["color"][2]);
-
-            float d = document["brightness"].as<float>();
-            renderer.setBrightness((int)(d * 255));
-        }
-    } else Serial.printf("HTTP request failed: %s\n", HTTPClient::errorToString(httpCode).c_str());
-
-    http.end();
 }
