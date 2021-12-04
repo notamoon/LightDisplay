b7f7beb973628690236c964d0103533f9217f74b
notamoon <mylessamuelharris@gmail.com>
2021-12-04 14:33:07 -0800

    conf template

---
+++ include/conf_template.h
@@ -0,0 +1,17 @@
+#ifndef LIGHTDISPLAY_CONF_H
+#define LIGHTDISPLAY_CONF_H
+
+namespace conf {
+
+    const char* networkSSID = "";
+    const char* networkPassword = "";
+
+    const char* serverURL = "";
+
+    const int displayWidth = 0, displayHeight = 0;
+
+    const int dataPin = 0;
+
+    const bool inverted = false;
+}
+#endif //LIGHTDISPLAY_CONF_H
