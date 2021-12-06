b3f8c659b0bda378acacd23450cecd27ac9bab4d
notamoon <mylessamuelharris@gmail.com>
2021-12-06 12:03:22 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,32 +16,23 @@ LightRenderer renderer(strip, conf::displayWidth, conf::displayHeight);
 void requestMessage();
 
 void setup() {
-    Serial.begin(115200);
+    renderer.setup();
 
-    Serial.print("Connecting to ");
-    Serial.println(conf::networkSSID);
+    renderer.setInverted(false);
+    renderer.setBrightness(255);
+
+    renderer.setFont(Fonts::FONT1);
+
+    renderer.setColor(Color::YELLOW);
+    renderer.scrollText("Setup", 0, 0, 1, -1, Direction::HORIZONTAL, 2);
+
+    renderer.update();
+    renderer.render();
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(conf::networkSSID, conf::networkPassword);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    renderer.setup();
-
-    renderer.setInverted(false);
-
-    renderer.setBrightness((int)(.25 * 255));
-
-    Serial.begin(115200);
+    while (WiFi.status() != WL_CONNECTED) delay(500);
 }
 
 unsigned long lastMessageRequest = 0;
@@ -49,25 +40,81 @@ unsigned long requestDelay = 5000;
 
 unsigned int color = 0;
 string message;
+string special;
 
 void loop() {
     if (lastMessageRequest + requestDelay < millis()) {
-        Serial.println("Sending message request.");
-
         requestMessage();
         lastMessageRequest = millis();
     }
 
     renderer.clear();
 
-    renderer.setColor(Color::CYAN);
+    if (special == "random") {
+        for (int x = 0; x < conf::displayWidth; x++) {
+            for (int y = 0; y < conf::displayHeight; y++) {
+                renderer.setColor(random(0, 256), random(0, 256), random(0, 256));
+                renderer.drawPixel(x, y);
+            }
+        }
+    }
+    else if (special == "horizontal_rainbow") {
+        double value = renderer.clock() * 8;
+        double increment = 360.0 / conf::displayWidth;
 
-    renderer.drawArea(0, 0, 30, 20);
+        for (int x = 0; x < conf::displayWidth; x++) {
+            for (int y = 0; y < conf::displayHeight; y++) {
+                Color::rgb color = Color::hsv2rgb(Color::hsv{fmod(value, 360.0), 1.0, 1.0});
 
-    renderer.setFont(Fonts::FONT2);
+                renderer.setColor(color.r * 255, color.g * 255, color.b * 255);
+                renderer.drawPixel(x, y);
+            }
 
-    renderer.setColor(color);
-    renderer.scrollText(message, 1, 4, 2, 28, Direction::HORIZONTAL, 2);
+            value += increment;
+        }
+    }
+    else if (special == "vertical_rainbow") {
+        double value = renderer.clock() * 8;
+        double increment = 360.0 / conf::displayHeight;
+
+        for (int y = 0; y < conf::displayHeight; y++) {
+            for (int x = 0; x < conf::displayWidth; x++) {
+                Color::rgb color = Color::hsv2rgb(Color::hsv{fmod(value, 360.0), 1.0, 1.0});
+
+                renderer.setColor(color.r * 255, color.g * 255, color.b * 255);
+                renderer.drawPixel(x, y);
+            }
+
+            value += increment;
+        }
+    }
+    else if (special == "rainbow") {
+        double value = renderer.clock() * 4;
+
+        for (int y = 0; y < conf::displayHeight; y++) {
+            for (int x = 0; x < conf::displayWidth; x++) {
+                Color::rgb color = Color::hsv2rgb(Color::hsv{fmod(value, 360.0), 1.0, 1.0});
+
+                renderer.setColor(color.r * 255, color.g * 255, color.b * 255);
+                renderer.drawPixel(x, y);
+            }
+        }
+
+        renderer.setFont(Fonts::FONT2);
+
+        renderer.setColor(0);
+        renderer.scrollText(message, 0, 4, 2, -1, Direction::HORIZONTAL, 2);
+    }
+    else {
+        renderer.setColor(Color::CYAN);
+
+        renderer.drawArea(0, 0, 30, 20);
+
+        renderer.setFont(Fonts::FONT2);
+
+        renderer.setColor(color);
+        renderer.scrollText(message, 1, 4, 2, 28, Direction::HORIZONTAL, 2);
+    }
 
     // Required
     renderer.update();
@@ -75,21 +122,13 @@ void loop() {
 }
 
 void requestMessage() {
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("Unable to connect to server!");
-        return;
-    }
+    if (WiFi.status() != WL_CONNECTED) return;
 
     WiFiClient client;
 
     HTTPClient http;
 
-    Serial.println("Sending HTTP Request");
-
-    if (!http.begin(client, conf::serverURL)) {
-        Serial.println("WiFi is not connected.");
-        return;
-    }
+    if (!http.begin(client, "http://10.73.227.190:4567/api")) return;
 
     // start connection and send HTTP header
     int httpCode = http.GET();
@@ -104,22 +143,18 @@ void requestMessage() {
 
             DeserializationError error = deserializeJson(document, payload);
 
-            if (error) {
-                Serial.print("Failed to deserialize JSON, error code: ");
-                Serial.println(error.f_str());
-                return;
-            }
-
-            //{"id":1,"message":"test","color":[255,255,255],"scroll":true,"speed":75,"displayWidth":20,"displayHeight":30,"brightness":0.25}
+            if (error) return;
 
             message = document["message"].as<const char*>();
 
             color = Adafruit_NeoPixel::Color(document["color"][0], document["color"][1], document["color"][2]);
 
+            special = document["special"].as<const char*>();
+
             float d = document["brightness"].as<float>();
             renderer.setBrightness((int)(d * 255));
         }
-    } else Serial.printf("HTTP request failed: %s\n", HTTPClient::errorToString(httpCode).c_str());
+    }
 
     http.end();
 }
