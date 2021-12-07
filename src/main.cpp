109a66038a2761b101ff370c4221318272de9da5
notamoon <mylessamuelharris@gmail.com>
2021-12-07 13:03:23 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,11 @@ LightRenderer renderer(strip, conf::displayWidth, conf::displayHeight);
 void requestMessage();
 
 void setup() {
+    Serial.begin(115200);
+
     renderer.setup();
 
-    renderer.setInverted(false);
+    renderer.setInverted(conf::inverted);
     renderer.setBrightness(255);
 
     renderer.setFont(Fonts::FONT1);
@@ -33,16 +35,56 @@ void setup() {
     WiFi.begin(conf::networkSSID, conf::networkPassword);
 
     while (WiFi.status() != WL_CONNECTED) delay(500);
+
+    Serial.println("Started");
 }
 
 unsigned long lastMessageRequest = 0;
-unsigned long requestDelay = 5000;
+unsigned long requestDelay = 10000;
 
-unsigned int color = 0;
-string message;
-string special;
+string type = "";
+int brightness;
+
+struct simple {
+    string message;
+    int textColor[3];
+    int borderColor[3];
+    int speed;
+};
+simple simpleData;
+
+struct twoline {
+    string topMessage;
+    int topTextColor[3];
+    int topSpeed;
+
+    string bottomMessage;
+    int bottomTextColor[3];
+    int bottomSpeed;
+
+    int borderColor[3];
+};
+twoline twolineData;
+
+struct image {
+    int image[30][20] = {};
+};
+twoline image;
+
+bool documentError = false;
+
+int freeHeap = ESP.getFreeHeap();
 
 void loop() {
+    //int loopStart = millis();
+
+    int currFreeHeap = ESP.getFreeHeap();
+
+    if (freeHeap != currFreeHeap) {
+        freeHeap = currFreeHeap;
+        //Serial.println(freeHeap);
+    }
+
     if (lastMessageRequest + requestDelay < millis()) {
         requestMessage();
         lastMessageRequest = millis();
@@ -50,85 +92,97 @@ void loop() {
 
     renderer.clear();
 
-    if (special == "random") {
-        for (int x = 0; x < conf::displayWidth; x++) {
-            for (int y = 0; y < conf::displayHeight; y++) {
-                renderer.setColor(random(0, 256), random(0, 256), random(0, 256));
-                renderer.drawPixel(x, y);
-            }
-        }
-    }
-    else if (special == "horizontal_rainbow") {
-        double value = renderer.clock() * 8;
-        double increment = 360.0 / conf::displayWidth;
+    if (documentError) {
+        renderer.setBrightness(255);
 
-        for (int x = 0; x < conf::displayWidth; x++) {
-            for (int y = 0; y < conf::displayHeight; y++) {
-                Color::rgb color = Color::hsv2rgb(Color::hsv{fmod(value, 360.0), 1.0, 1.0});
+        if (renderer.clock() / 10 % 2 == 1) renderer.setColor(Color::YELLOW);
+        else renderer.setColor(0);
 
-                renderer.setColor(color.r * 255, color.g * 255, color.b * 255);
-                renderer.drawPixel(x, y);
-            }
-
-            value += increment;
-        }
-    }
-    else if (special == "vertical_rainbow") {
-        double value = renderer.clock() * 8;
-        double increment = 360.0 / conf::displayHeight;
-
-        for (int y = 0; y < conf::displayHeight; y++) {
-            for (int x = 0; x < conf::displayWidth; x++) {
-                Color::rgb color = Color::hsv2rgb(Color::hsv{fmod(value, 360.0), 1.0, 1.0});
-
-                renderer.setColor(color.r * 255, color.g * 255, color.b * 255);
-                renderer.drawPixel(x, y);
-            }
-
-            value += increment;
-        }
-    }
-    else if (special == "rainbow") {
-        double value = renderer.clock() * 4;
-
-        for (int y = 0; y < conf::displayHeight; y++) {
-            for (int x = 0; x < conf::displayWidth; x++) {
-                Color::rgb color = Color::hsv2rgb(Color::hsv{fmod(value, 360.0), 1.0, 1.0});
-
-                renderer.setColor(color.r * 255, color.g * 255, color.b * 255);
-                renderer.drawPixel(x, y);
-            }
-        }
+        renderer.drawArea(0, 0, conf::displayWidth, conf::displayHeight);
 
         renderer.setFont(Fonts::FONT2);
 
-        renderer.setColor(0);
-        renderer.scrollText(message, 0, 4, 2, -1, Direction::HORIZONTAL, 2);
+        renderer.setColor(Color::RED);
+
+        renderer.scrollText("Error", 1, 4, 2, 28, Direction::HORIZONTAL, 3);
     }
     else {
-        renderer.setColor(Color::CYAN);
+        renderer.setBrightness(brightness);
 
-        renderer.drawArea(0, 0, 30, 20);
+        if (type == "simple") {
+            renderer.setColor(simpleData.borderColor[0], simpleData.borderColor[1], simpleData.borderColor[2]);
 
-        renderer.setFont(Fonts::FONT2);
+            renderer.drawArea(0, 0, conf::displayWidth, conf::displayHeight);
 
-        renderer.setColor(color);
-        renderer.scrollText(message, 1, 4, 2, 28, Direction::HORIZONTAL, 2);
+            renderer.setFont(Fonts::FONT2);
+
+            renderer.setColor(simpleData.textColor[0], simpleData.textColor[1], simpleData.textColor[2]);
+
+            renderer.scrollText(simpleData.message, 1, 4, 2, 28, Direction::HORIZONTAL, simpleData.speed);
+        } else if (type == "twoline") {
+            /*
+            renderer.setBrightness(document["data"]["brightness"].as<int>());
+
+            renderer.setColor(document["data"]["borderColor"][0].as<int>(), document["data"]["borderColor"][1].as<int>(),
+                              document["data"]["borderColor"][2].as<int>());
+
+            renderer.drawArea(0, 0, conf::displayWidth, conf::displayHeight);
+
+            renderer.setFont(Fonts::FONT1);
+
+            renderer.setColor(document["data"]["topTextColor"][0].as<int>(), document["data"]["topTextColor"][1].as<int>(),
+                              document["data"]["topTextColor"][2].as<int>());
+
+            renderer.scrollText(document["data"]["topMessage"].as<string>(), 1, 2, 2, 28, Direction::HORIZONTAL, document["data"]["topSpeed"].as<int>());
+
+            renderer.setColor(document["data"]["bottomTextColor"][0].as<int>(),
+                              document["data"]["bottomTextColor"][1].as<int>(),
+                              document["data"]["bottomTextColor"][2].as<int>());
+
+            int bottomSpeed = document["data"]["bottomScroll"].as<bool>() ? document["data"]["bottomSpeed"].as<int>() : 0;
+
+            renderer.scrollText(document["data"]["bottomMessage"].as<string>(), 1, 2, 9, 28, Direction::HORIZONTAL,
+                                bottomSpeed);
+                                */
+        } else if (type == "image") {
+            /*
+            renderer.setBrightness(document["data"]["brightness"].as<int>());
+
+            for (auto x: document["data"]["image"].as<JsonArray>()) {
+                for (auto y: x.as<JsonArray>()) {
+                    renderer.setColor(y[0].as<int>(), y[1].as<int>(), y[2].as<int>());
+                }
+            }
+             */
+        } else {
+            renderer.setFont(Fonts::FONT1);
+
+            renderer.setColor(Color::RED);
+            renderer.scrollText("Unknown Input Type", 0, 0, 1, -1, Direction::HORIZONTAL, 2);
+        }
     }
 
     // Required
     renderer.update();
     renderer.render();
+
+    Serial.println(ESP.getFreeHeap());
+
+    //Serial.println(millis() - loopStart);
 }
 
 void requestMessage() {
     if (WiFi.status() != WL_CONNECTED) return;
 
+    yield();
+
+    Serial.println("Requesting message");
+
     WiFiClient client;
 
     HTTPClient http;
 
-    if (!http.begin(client, "http://10.73.227.190:4567/api")) return;
+    if (!http.begin(client, "http://mvhscsf.ml:4567/api")) return;
 
     // start connection and send HTTP header
     int httpCode = http.GET();
@@ -137,22 +191,36 @@ void requestMessage() {
     if (httpCode > 0) {
 
         if (httpCode == HTTP_CODE_OK) {
-            String payload = http.getString();
 
-            DynamicJsonDocument document(400);
+            StaticJsonDocument<800> document;
 
-            DeserializationError error = deserializeJson(document, payload);
+            DeserializationError error = deserializeJson(document, http.getString());
 
-            if (error) return;
+            if (error) {
+                documentError = true;
+                document.clear();
+                return;
+            }
 
-            message = document["message"].as<const char*>();
+            type = document["type"].as<const char*>();
+            brightness = document["data"]["brightness"].as<int>();
 
-            color = Adafruit_NeoPixel::Color(document["color"][0], document["color"][1], document["color"][2]);
+            if (type == "simple") {
+                simpleData.message = document["data"]["message"].as<const char*>();;
 
-            special = document["special"].as<const char*>();
+                simpleData.textColor[0] = document["data"]["textColor"][0].as<int>();
+                simpleData.textColor[1] = document["data"]["textColor"][1].as<int>();
+                simpleData.textColor[2] = document["data"]["textColor"][2].as<int>();
 
-            float d = document["brightness"].as<float>();
-            renderer.setBrightness((int)(d * 255));
+                simpleData.borderColor[0] = document["data"]["borderColor"][0].as<int>();
+                simpleData.borderColor[1] = document["data"]["borderColor"][1].as<int>();
+                simpleData.borderColor[2] = document["data"]["borderColor"][2].as<int>();
+
+                simpleData.speed = document["data"]["speed"].as<int>();
+            }
+
+            document.clear();
+
         }
     }
 
